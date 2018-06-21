3b8d8e3e12cbff14f6a3b82e331df22dca065872
王士哲 <S352431@yo-woo.com>
2018-06-21 18:47:55 +0800

    remove Service

--- src/main/java/com/aidan/basetools/models/network/ServiceCallback.java
+++
@@ -1,9 +0,0 @@
-package com.aidan.basetools.models.network;
-
-/**
- * Created by Aidan on 2018/3/10.
- */
-
-public interface ServiceCallback<T> {
-    void onResult(boolean success, T list, ServiceConstants.ErrorHandler errorHandler);
-}
